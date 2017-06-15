49d49d86ada157b006231226df2b941d33483c92
Tbryant <waterstrok@aol.com>
2017-06-14 17:42:31 -0700

    It will be clicking 3 variations at once, not what we want

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 $(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
+//
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+  var clickables = ["header", "paragraph", "img"];
+  // var addingOne = 0
+    $("#header,#paragraph,#img").click(function(event) {
+      clickables.forEach(function(click) {
+        var clicking = $("." + click).val();
+        //   console.log(click);
+          $("." + click).text(clicking);
+          alert("This is the: " + click);
+        //     alert("poop: " + click);
+        //     alert("fart: " + click);
+        //     alert("shit: " + click);
+
+        // if (clickables = "header") {
+        //     alert("This is the: " + click);
+        //
+        // } else if (clickables = "paragraph") {
+        //     alert("This is the:" + click);
+        // } else {
+        //     alert("This is the: " + click);
+        // }
   });
 
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
+      event.preventDefault();
   });
 });
+//
+// $("#blanks form").submit(function(event) {
+//     var blanks = ["favDog","favFruit","favColor","favPlace"];
+//     blanks.forEach(function(blank) {
+//
+//       var userInput = $("input#" + blank).val();
+//       console.log(blank);
+//       $("." + blank).text(userInput)
+//      });
